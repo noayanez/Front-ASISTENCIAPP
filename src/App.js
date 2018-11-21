3c4a52736aec24f5519e5b577006a98dda081630
noayanez <kmylow.10@gmail.com>
2018-11-21 12:36:12 -0500

    añadido estilos

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ class App extends Component {
     constructor(){
         super();
         this.state={
+            hostname : "127.0.0.1:5000",// 165.227.168.49:5000
             dataUsuario : [],
             horaactual : null,
             logueado : false,
@@ -13,13 +14,15 @@ class App extends Component {
             numAsistencias : 10,
             numTardanzas : 2,
             numFaltas : 1,
-            alerta : ""
+            alerta : "",
+            asistido : false,
         }
         this.login = this.login.bind(this);
         this.logout = this.logout.bind(this);
         this.cambiarcodigo = this.cambiarcodigo.bind(this);
         this.fetchUsuario = this.fetchUsuario.bind(this);
         this.marcarAsistencia = this.marcarAsistencia.bind(this);
+        this.marcarAsistencia2 = this.marcarAsistencia2.bind(this);
         this.cerrarAlerta = this.cerrarAlerta.bind(this);
     }
 
@@ -28,7 +31,7 @@ class App extends Component {
     }
 
     logout(){
-        this.setState({ logueado : false });
+        this.setState({ logueado : false , alerta : "", asistido : false });
     }
 
     cambiarcodigo(event){
@@ -40,10 +43,17 @@ class App extends Component {
     }
 
     marcarAsistencia(){
+        this.setState({
+            asistido : true,
+            alerta : "¡Asistencia marcada axitosamente a las "+this.state.horaactual
+        })
+    }
+
+    marcarAsistencia2(){
         const data = {
             dni : this.state.dataUsuario.UsuarioTrabajador.dni
         };
-        fetch("http://127.0.0.1:5000/usuariotrabajador/"+this.state.codigoUsuario,{
+        fetch("http://"+this.state.hostname+":5000/usuariotrabajador/"+this.state.codigoUsuario,{
             method : 'POST',
             headers : {
                 'Accept' : '*/*',
@@ -72,7 +82,7 @@ class App extends Component {
         this.setState({
             logueado : false
         });
-        fetch("http://127.0.0.1:5000/usuariotrabajador/"+this.state.codigoUsuario)
+        fetch("http://"+this.state.hostname+"/usuariotrabajador/"+this.state.codigoUsuario)
         .then((response) =>{
             return response.json();
         })
@@ -115,82 +125,82 @@ class App extends Component {
     render() {
         return (
             <div className="App">
-                <div className="container-fluid">
-                    <div className="row">
-                        <div className="col-2"></div>
-                        <div className="col-8">
-                            {this.state.logueado===true?
-                                (
-
+                <header className="App-header">
+                    {this.state.logueado === true ?
+                        (
+                            <div className="MiCaja">
+                                <div className="Caja">
+                                    <div className="image"></div>
+                                    <h1>{this.state.horaactual}</h1>
+                                </div>
+                                <div className="Caja">
+                                    <h2>¡Buenos días {this.state.dataUsuario.UsuarioTrabajador.nombre}!</h2>
                                     <div>
-                                        <br/><br/><br/><br/><br/><br/><br/>
-                                        <div className="row">
-                                            <div className="col-12">
-                                                <h1>{this.state.horaactual}</h1>
-                                            </div>
-                                        </div>
-                                        <div className="row">
-                                            <div className="col-12">
-                                                <h2>¡Buenos días {this.state.dataUsuario.UsuarioTrabajador.nombre}!</h2>
-                                            </div>
-                                        </div>
-                                        <div className="row">
-                                            <div className="col-12">
-                                                <br/>
-                                                <button className="btn btn-outline-success" onClick={this.marcarAsistencia}>MARCAR ASISTENCIA</button>
-                                            </div>
-                                        </div>
+                                        {!this.state.asistido?(<button className="mybtn" onClick={this.marcarAsistencia}>MARCAR ASISTENCIA
+                                        </button>):(<button className="mybtn" onClick={this.logout}>SALIR DE SESION
+                                        </button>)}
                                     </div>
-                                ):(
-                                    <div className="row">
-                                        <div className="col-12">
-                                            <div className="row">
-                                                <div className="col-3"></div>
-                                                <div className="col-6">
-                                                    <div className="row">
-                                                        <div className="col">
-                                                            <label>Ingresa tu codigo o DNI:</label>
-                                                        </div>
-                                                    </div>
-                                                    <div className="row">
-                                                        <div className="col-3"></div>
-                                                        <div className="col-6">
-                                                            <input className="form-control" id="campodni" type="text" value={this.state.codigoUsuario} onChange={this.cambiarcodigo} maxLength={8}/>
-                                                        </div>
-                                                        <div className="col-3"></div>
-                                                    </div>
-                                                    <div className="row">
-                                                        <div className="col">
-                                                            <br/>
-                                                            <button className="btn btn-outline-success" onClick={this.fetchUsuario}>ENTRAR</button>
-                                                        </div>
-                                                    </div>
-                                                    {this.state.alerta!==""?
-                                                        (<div className="row">
-                                                            <div className="col-12">
-                                                                <div className="alert alert-primary alert-dismissible fade show" role="alert">
-                                                                    {this.state.alerta}
-                                                                    <button onClick={this.cerrarAlerta} type="button" className="close" data-dismiss="alert" aria-label="Close">
-                                                                        <span aria-hidden="true">&times;</span>
-                                                                    </button>
-                                                                </div>
-                                                            </div>
-                                                        </div>):(null)
-                                                    }
+                                    {this.state.alerta !== "" ?
+                                        (<div className="row">
+                                            <div className="col-12">
+                                                <br/><div className="alert alert-success alert-dismissible fade show"
+                                                     role="alert">
+                                                    {this.state.alerta}
+                                                    <button onClick={this.cerrarAlerta} type="button" className="close"
+                                                            data-dismiss="alert" aria-label="Close">
+                                                        <span aria-hidden="true">&times;</span>
+                                                    </button>
                                                 </div>
-                                                <div className="col-3"></div>
                                             </div>
+                                        </div>) : (null)
+                                    }
+                                </div>
+                            </div>
+                        ) : (
+                            <div className="login">
+                                <div className="col-sm-6 col-sm-offset-3 myform-cont">
+                                    <div className="myform-top">
+                                        <div className="col-sm-12 my-sesion">
+                                            <h1> Inicio de sesion </h1>
+                                        </div>
+                                        <div className="myform-top-left">
+                                            <p>Ingresa tu codigo o DNI </p>
                                         </div>
                                     </div>
-                                )
-                            }
-                        </div>
-                        <div className="col-2"></div>
-                    </div>
-                </div>
+                                    <div className="myform-bottom">
+                                        <div>
+                                            <div className="form-group">
+                                                <input className="form-control" id="campodni" type="text"
+                                                       value={this.state.codigoUsuario} onChange={this.cambiarcodigo}/>
+                                            </div>
+                                            <br/>
+                                            <button className="mybtn" onClick={this.fetchUsuario}>ENTRAR</button>
+
+                                        </div>
+                                        {this.state.alerta !== "" ?
+                                            (<div className="row">
+                                                <div className="col-12">
+                                                    <br/><div className="alert alert-primary alert-dismissible fade show"
+                                                         role="alert">
+                                                        {this.state.alerta}
+                                                        <button onClick={this.cerrarAlerta} type="button" className="close"
+                                                                data-dismiss="alert" aria-label="Close">
+                                                            <span aria-hidden="true">&times;</span>
+                                                        </button>
+                                                    </div>
+                                                </div>
+                                            </div>) : (null)
+                                        }
+                                    </div>
+                                </div>
+                            </div>
+                        )
+                    }
+                </header>
             </div>
         );
     }
+
 
 }
 
